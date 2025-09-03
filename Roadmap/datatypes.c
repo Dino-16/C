0b64ec0743cf3751b46d5b7e017990abf5ee1e48
Dino-16 <ceejaycutie11@gmail.com>
2025-09-03 17:33:03 +0800

    2

---
+++ Roadmap/datatypes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // Datatypes - are types of values. It has:
+
+    // int - for interges / whole numbers.
+    // float - for 32 bits size/ less precise.
+    // double - for 16 decimal values/ more precise.
+    // char - for a character / 1 letter at a time.
+
+    int myNum = 9;
+    float myFloat = 9.33;
+    double myDouble = 9.33;
+    char myChar = 'a';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%lf\n", myDouble);
+    printf("%c\n", myChar);
+
+}
